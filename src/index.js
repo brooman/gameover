92587605c4b602c3f724ab6dd7e879cd1511d1bf
André Broman <bromanandre@gmail.com>
2019-05-03 15:25:14 +0200

    Changed to p5js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-const phaser = require('phaser')
+const p5 = require('p5')
+const player = require('./objects/player')
+
+setup = () => {
+  createCanvas(800, 800)
+  background(0)
+}
+
+draw = () => {
+
+}
---
+++ src/objects/player/index.js
@@ -0,0 +1,16 @@
+class Player {
+
+  construct(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 0;
+  }
+
+  update() {
+
+  }
+
+  show () {
+
+  }
+}
