b5d73f8d8f70fbc2dcc4ac983c84ab46d3d6345d
André Broman <bromanandre@gmail.com>
2019-05-03 14:51:18 +0200

    Socket.IO setup

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const path = require('path');
 const express = require('express')
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use('/static', express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.use('/static', express.static('public'))
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
-
+server.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
