c06a6f82b29969748eaf5fc6c42d2a4d61318753
Samuel <sumlebulle@hotmail.com>
2019-05-14 14:26:10 +0200

    Change getRandomColor to get from an array. Added getRandomColor to player. Change background color

--- src/classes/player/index.js
+++ src/classes/player/index.js
@@ -6,7 +6,26 @@ class Player {
 
     this.speed = 3
     this.radius = 10
+    this.color = this.getRandomColor()
   }
+
+  getRandomColor() {
+
+    let colors = [
+        {r: 218, g: 252, b: 82},
+        {r: 231, g: 61, b: 247},
+        {r: 235, g: 53, b: 62},
+        {r: 42, g: 104, b: 156},
+        {r: 117, g: 250, b: 138},
+        {r: 234, g: 55, b: 143},
+        {r: 234, g: 55, b: 132},
+     
+    ]
+
+    const c = colors[Math.floor(Math.random() * colors.length)]
+
+    return color(c.r, c.g, c.b)
+}
 
   updatePosition(x, y) {
     this.x = x
@@ -14,7 +33,7 @@ class Player {
   }
 
   show(x, y) {
-    fill(100, 200, 500)
+    fill(this.color)
     circle(x, y, this.radius * 2)
   }
 
